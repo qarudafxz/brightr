595f8fdf8eda9d425086954c8f329476f373da6b
GarudaFX <email@francistinao.tech>
2023-11-29 01:15:29 +0800

    done. will improve the ui later

--- api/main.py
+++ api/main.py
@@ -1,79 +1,65 @@
 # Import necessary libraries
 from flask import Flask, request, jsonify
-import base64
+from flask_cors import CORS
 import cv2
-import numpy as np
-from tensorflow import keras
-from tensorflow.keras import layers
+import os
+import base64
 
 app = Flask(__name__)
+CORS(app)
 
-height = 128
-width = 128
-channels = 3
-
-# api endpoint to test the server
-@app.route('/api/adjust_hsl', methods=['POST'])
-def adjust_hsl_endpoint():
-    data = request.json
-
-    # extract file paths from the request data
-    file_paths = data.get('file_paths', [])
-
-    # convert file paths to numpy arrays
-    uploaded_images_np = [cv2.imread(file_path) for file_path in file_paths]
-
-    # converting images to HSL format
-    hsl_images = [cv2.cvtColor(img, cv2.COLOR_BGR2HLS) for img in uploaded_images_np]
-
-    #adjusting the original hsl values of image to the hsl values returned from regression model
-    adjusted_images = adjust_hsl_values(hsl_images, model)
-    
-    adjusted_images_base64 = [cv2.imencode('.jpg', img)[1].tobytes().decode('utf-8') for img in adjusted_images]
-
-    # Return adjusted images to the frontend
-    return jsonify(adjusted_images_base64)
-
-# read images from file paths
-def read_images(file_paths):
-    images = [cv2.imread(file_path) for file_path in file_paths]
-    return images
-
-# adjust HSL values of an image
-def adjust_hsl_values(hsl_imgs, model):
-    adjusted_hsl_imgs = []
-    
-    for img in hsl_imgs:
-        h, l, s = cv2.split(img)
-    
-        img_array = np.array([h, l, s]).reshape(1, height, width, 3)
-    
-        predicted_hsl = model.predict(img_array)
-    
-        predicted_h, predicted_s, predicted_l = predicted_hsl[0]
-        
-        h = h + predicted_h
-        s = s + predicted_s
-        l = l + predicted_l
+@app.route('/api/adjust_brightness', methods=['POST'])
+def adjust_brightness_endpoint():
+    try:
   
-        h = np.clip(h, 0, 255)
-        s = np.clip(s, 0, 255)
-        l = np.clip(l, 0, 255)
-        
-        adjusted_img = cv2.merge((h, l, s))
-        adjusted_hsl_imgs.append(adjusted_img)
+        uploaded_images = request.files.getlist('images')
+
+
+        file_paths = [save_uploaded_file(image) for image in uploaded_images]
+
     
-    return adjusted_hsl_imgs
+        adjusted_images = [adjust_brightness(preprocess_image(file_path)) for file_path in file_paths]
 
-# Regression model
-model = keras.Sequential([
-    layers.Conv2D(64, (3, 3), activation='relu', input_shape=(height, width, channels)),
-    layers.Flatten(),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(3)  # Output layer for HSL values
-])
+        adjusted_images_base64 = [image_to_base64(img) for img in adjusted_images]
 
-model.compile(optimizer='adam', loss='mean_squared_error')
+   
+        for i, adjusted_img in enumerate(adjusted_images):
+            cv2.imwrite(file_paths[i], adjusted_img)
+        
+
+        return jsonify(adjusted_images_base64, {'success': 'Images adjusted successfully!'})
+    except Exception as e:
+        print(e)
+        return jsonify({'error': 'Something went wrong!'})
+
+def save_uploaded_file(file):
+    file_path = os.path.join("../uploads", file.filename)
+    file.save(file_path)
+    return file_path
+
+def preprocess_image(file_path):
+    # Read and resize the image to the expected size
+    img = cv2.imread(file_path)
+    return img
+
+def adjust_brightness(img):
+    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    
+    l, a, b = cv2.split(lab)
+    
+    # Adjust the lightness channel
+    l = cv2.add(l, 40)
+    
+    # Adjust the contrast (curve) of the lightness channel
+    l = cv2.convertScaleAbs(l, alpha=1.2, beta=10) 
+    
+    adjusted_img = cv2.merge((l, a, b))
+    
+    return cv2.cvtColor(adjusted_img, cv2.COLOR_LAB2BGR)
+
+def image_to_base64(img):
+    # Convert image to base64 for easy transfer to the frontend
+    return base64.b64encode(cv2.imencode('.jpg', img)[1]).decode('utf-8')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,83 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React, { useState } from "react";
+import { IoIosImages } from "react-icons/io";
 
 function App() {
- const [count, setCount] = useState(0);
+ const [images, setImages] = useState<File[]>([]);
+ const [brightenedImages, setBrightenedImages] = useState<string[]>([]);
+
+ const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  if (e.target.files) {
+   const filesArray = Array.from(e.target.files);
+   setImages(filesArray);
+  }
+ };
+
+ const handleSubmit = async (e: React.MouseEvent) => {
+  e.preventDefault();
+  try {
+   const formData = new FormData();
+   images.forEach((image) => {
+    formData.append("images", image);
+   });
+
+   const res = await fetch("http://127.0.0.1:5000/api/adjust_brightness", {
+    method: "POST",
+    body: formData,
+   });
+
+   const data = await res.json();
+   setBrightenedImages(data[0]);
+  } catch (err) {
+   console.log(err);
+  }
+ };
 
  return (
-  <>
-   <div>
-    <a href="https://vitejs.dev" target="_blank">
-     <img src={viteLogo} className="logo" alt="Vite logo" />
-    </a>
-    <a href="https://react.dev" target="_blank">
-     <img src={reactLogo} className="logo react" alt="React logo" />
-    </a>
+  <div className="h-full py-4 font-main grid justify-center place-content-center place-items-center items-center">
+   <label className="custom-file-input bg-white border border-gray-300 rounded-lg p-14 cursor-pointer flex flex-col place-items-center">
+    <input
+     type="file"
+     onChange={handleFileChange}
+     multiple
+     className="hidden"
+    />
+    <IoIosImages className="text-6xl text-center" />
+    <span className="text-center mt-4 text-zinc-500">Upload images</span>
+   </label>
+   <button
+    className="font-bold text-sm text-center py-2 rounded-md bg-blue-600 px-4 text-white mt-4"
+    onClick={(e) => handleSubmit(e)}
+   >
+    Adjust Brightness
+   </button>
+   <div className="grid grid-cols-3 gap-4 mt-4">
+    {images.map((image) => (
+     <img
+      key={image.name}
+      src={URL.createObjectURL(image)}
+      alt="preview"
+      className="w-32 h-32 object-cover"
+     />
+    ))}
    </div>
-   <h1>Vite + React</h1>
-   <div className="card">
-    <button onClick={() => setCount((count) => count + 1)}>
-     count is {count}
-    </button>
-    <p>
-     Edit <code>src/App.tsx</code> and save to test HMR
-    </p>
-   </div>
-   <p className="read-the-docs">
-    Click on the Vite and React logos to learn more
-   </p>
-  </>
+
+   {brightenedImages.length !== 0 && (
+    <div className="border border-zinc-400 p-4 rounded-md mt-4">
+     <h1 className="font-bold text-4xl text-center my-3">Brightened Images</h1>
+
+     <div className="grid grid-cols-3 gap-4">
+      {brightenedImages.map((image, idx) => (
+       <img
+        key={idx}
+        alt="preview"
+        className="w-32 h-32 object-cover"
+        src={`data:image/jpeg;base64,${image}`}
+       />
+      ))}
+     </div>
+    </div>
+   )}
+  </div>
  );
 }
 
